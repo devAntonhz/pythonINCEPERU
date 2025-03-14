e68961c1e538456d06afe9b3a67fe645bc24e65d
Antonio Ramirez <est.010116532@ismontessori.edu.pe>
2025-03-13 22:57:24 -0500

    CLASE 01: INTRODUCCION INCEPERU

---
+++ 01-Introduccion/01-comentarios.py
@@ -0,0 +1,19 @@
+#
+# ? COMENTARIOS
+
+# ? COMENTARIO DE LINEA
+print("Hola Mundo")  # ? IMPRIME HOLA MUNDO
+
+# ? MULTIPLES LINEAS
+"""
+Aprender python
+es muy 
+Chevere
+"""
+
+# CTRL + } = Comenta el codigo
+# print("Pepito Cardenas")
+# print("Pepito Cardenas")
+# print("Pepito Cardenas")
+# print("Pepito Cardenas")
+# print("Pepito Cardenas")
---
+++ 01-Introduccion/02-variables.py
@@ -0,0 +1,26 @@
+#
+# ? VARIABLES
+# nombre = "Antonio Ramirez"
+# print(nombre)
+
+# # ? FORMAS DE IMPRIMIR
+# # ? CADENAS FORMAT
+# print("{}".format(nombre))
+
+# # ? CADENAS F
+# print(f"{nombre}")
+
+nombre = "Pepito Chacalito"
+apellido = "Chavez"
+
+print("Su nombre es:", nombre, "Su apellido es:", apellido)
+
+print("Su nombre es: {} Su apellido es: {}".format(nombre, apellido))
+
+print(f"Su nombre es: {nombre} Su apellido es: {apellido}")
+
+# ! TAREA: IMPRIME EL DATO DE UNA PERSONA DE: (Nomre, Edad, Telefono), para nombre: usar Print, para edad: usar .format, para telefono usar f String.
+# ! EJEMPLO EN CONSOLA:
+# ! Su nombre es: xxxxx
+# ! Su edad es: xxx
+# ! Su numero de telefono es: 9xxxxxx
