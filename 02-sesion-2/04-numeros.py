3a4cb445e50aff08620ac1f59f8dbb7f2007af5d
Antonio Ramirez <est.010116532@ismontessori.edu.pe>
2025-03-18 23:17:09 -0500

    Clase 02

---
+++ 02-sesion-2/04-numeros.py
@@ -0,0 +1,22 @@
+# 
+# ? INTEGERS
+x = 20
+print(x)
+
+y = -50
+print(y)
+
+z = 4 + 5j
+print(z)
+print(type(z))
+
+ahorro = -1_564_567_489_189
+ahorro = ahorro + 500
+print(ahorro)
+
+extenso = 56e50
+print(extenso)
+
+# ? FLOAT: Lo mismo a los enteros pero con decimales
+extenso_decimal = 56.24e20
+print(extenso_decimal)
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print("Hola Mundo")
+# print("Hola Mundo")
 
-# ! TAREA: IMPRIME EN CONSOLA DE PYTHON EL DATO DE UNA PERSONA CON NOMBRES COMPLETOS
+# # ! TAREA: IMPRIME EN CONSOLA DE PYTHON EL DATO DE UNA PERSONA CON NOMBRES COMPLETOS
+
+# ! EJERCICIO: A ESTOS DAOS CAMBIA:
+# ! A LA PRIMERA VARIABLE que conviertela a MAYUSCULA, a la
+#segunda en MINUSCULA, y a la TERCERA, Solo las primeras letras en
+#mayusculas, Cambia el apellido a estos datos.
+
